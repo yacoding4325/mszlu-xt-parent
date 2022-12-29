67929a4819b6b78f5c09f5c68683a23f33a0f41e
yxym0203 <1500081802@qq.com>
2022-12-29 17:15:20 +0800

    添加配置信息 以及扫包路径

---
+++ mszlu-xt-web/mszlu-xt-web-api/src/main/java/com/mszlu/xt/web/config/MybatisPlusConfig.java
@@ -0,0 +1,19 @@
+package com.mszlu.xt.web.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.mszlu.xt.web.dao")
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
