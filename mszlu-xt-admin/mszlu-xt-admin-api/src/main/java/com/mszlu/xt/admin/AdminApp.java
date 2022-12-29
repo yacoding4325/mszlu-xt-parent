aa6a24dc2fd7034b11a4d403fb7bdd31340f23e5
yxym0203 <1500081802@qq.com>
2022-12-29 19:48:55 +0800

    管理台登录功能

---
+++ mszlu-xt-admin/mszlu-xt-admin-api/src/main/java/com/mszlu/xt/admin/AdminApp.java
@@ -0,0 +1,18 @@
+package com.mszlu.xt.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author yaCoding
+ * @create 2022-12-29 下午 7:34
+ */
+
+@SpringBootApplication
+public class AdminApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApp.class,args);
+    }
+
+}
