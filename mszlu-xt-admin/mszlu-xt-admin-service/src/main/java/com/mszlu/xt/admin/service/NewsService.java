c4879caaace2c3c326881fda8f06c4ba05593d1d
yxym0203 <1500081802@qq.com>
2022-12-29 20:30:04 +0800

    管理台新闻管理

---
+++ mszlu-xt-admin/mszlu-xt-admin-service/src/main/java/com/mszlu/xt/admin/service/NewsService.java
@@ -0,0 +1,19 @@
+package com.mszlu.xt.admin.service;
+
+import com.mszlu.xt.admin.params.NewsParam;
+import com.mszlu.xt.common.model.CallResult;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface NewsService {
+
+    CallResult findPage(NewsParam newsParam);
+
+    CallResult save(NewsParam newsParam);
+
+    CallResult findNewsById(NewsParam newsParam);
+
+    CallResult update(NewsParam newsParam);
+
+    CallResult upload(MultipartFile file);
+
+}
